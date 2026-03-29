f115ba4071c26556b631623d5f14371dce3ba13e
Ayoub <ayoubiss.10@gmail.com>
2026-03-29 18:17:12 +0100

    feat: finalize initial setup and customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export default function Home() {
   return (
-    <Button variant="destructive">
-      Click me 
+    <Button
+      onClick={() =>
+        toast("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        })
+      }
+      variant="default"
+    >
+      Click me
     </Button>
   );
 }
